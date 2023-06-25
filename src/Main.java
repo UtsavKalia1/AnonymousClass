e58f352c2faab07f27da8fde864b4ef135189044
UtsavKalia1 <utsavkalia66@gmail.com>
2023-06-25 16:03:54 -0400

    working with lamdas and funactional interfaces

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,9 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.function.Consumer;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -17,5 +23,45 @@ public class Main {
     // with lamda we can only have implementation of
         //abstarct class(functional interface
         System.out.println(calInterestlamda1.SimpleInterest(2, 3.5, 100, 60));
+
+        List<String> names = Arrays.asList("Utsav", "Sam", "Varun");
+
+
+//        Comparator callll = new Comparator() {
+//            @Override
+//            public int compare(Object o1, Object o2) {
+//                return 0;
+//            }
+//        };
+
+//        default void sort(Comparator<? super E> c) {
+//            Object[] a = this.toArray();
+//            Arrays.sort(a, (Comparator) c);
+//            ListIterator<E> i = this.listIterator();
+//            for (Object e : a) {
+//                i.next();
+//                i.set((E) e);
+//            }
+//        }
+        names.sort((x,y) -> x.compareTo(y));
+        System.out.println(names);
+        Consumer cc = new Consumer() {
+            @Override
+            public void accept(Object o) {
+
+            }
+        };
+        names.forEach((s) -> System.out.println("Hello "+s + "."));
+
+
     }
+
+
+// using lamda for interface
+
+    //Comparator is functional
+    //interface
+    //comparable is not functional
+    //interface
+    //methos is compareto -> comparable
 }
